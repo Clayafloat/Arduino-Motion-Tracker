54dea97fa5bd0fad2676142124b03c4aca576f2b
Clayton Halim <ClaytonHalim3@gmail.com>
2016-07-28 16:11:36 -0400

    Add files via upload

---
+++ Trimmer.py
@@ -0,0 +1,40 @@
+def main(path, newFile):
+"""
+Processes and cleans the raw data collected from the arduino into a csv 
+"""
+    f = open(path, 'r')
+    w = open(newFile + ".csv", 'w')
+
+    record = {}
+    prevTime = 0.0
+    largest = -9999999.9
+    temp = 0.0
+
+    subtract = float(f.readline().split(',')[0])
+
+    data = list(f)
+
+    subtract = subtract - 0.001
+
+    for line in data:
+        data = line.split(",")
+        pos = float(data[1])
+
+        if pos > largest and pos > temp:
+            largest = pos
+            prevTime = data[0]
+        elif pos == largest and pos:
+            temp = pos
+        else:
+            if (largest != -9999999.9 and float(prevTime) <= 120.0):
+                w.writelines(str(float(prevTime) - subtract) + "," + str(largest) + "\n")
+                decay = largest
+            largest = -9999999.9
+            temp = 0.0
+            
+    f.close()
+    w.close()
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1], sys.argv[2])
---
+++ sonar.ino
@@ -0,0 +1,111 @@
+#include <NewPing.h>
+
+const int TRIG = 8;
+const int ECHO = 7;
+const int BUTTON = 2;
+const int LED = 6;
+const int GO_LED = 3;
+const int TMPPIN = A5;
+
+double VELOCITY = 0; // Initialized in setup
+
+double TEMP = 0; // Celcius, initialized in setup
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+unsigned long totalMillis = 0;
+double previousDistance = 0.0;
+double equilibrium = 0.0;
+
+int switchState = 0;
+
+NewPing sonar(TRIG, ECHO, 200);
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(GO_LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+
+  for (int i = 0; i < 99; ++i)
+  {
+     TEMP += tempC();
+  }
+
+  TEMP /= 100; // Find avg temp from 100 readings
+  VELOCITY = 331.5 + (0.6 * TEMP);
+  Serial.print(TEMP);
+  Serial.println(" C");
+}
+
+void loop() 
+{
+  currentMillis = millis();
+  
+  if (digitalRead(BUTTON) == HIGH)
+  {
+    switchState = 1 - switchState;
+
+    if (switchState == 1)
+    {
+      Serial.println("COMMAND: start_recording");
+      equilibrium = distance();
+      digitalWrite(LED, HIGH);
+      delay(100);
+    }
+    else if (switchState == 0)
+    {
+      Serial.println("COMMAND: stop_recording");
+      totalMillis = 0.0;
+      digitalWrite(LED, LOW);
+      delay(100);
+    }
+  }
+
+  double distanceD = distance();
+
+  if (switchState == 1 && distanceD != previousDistance)
+  {
+    previousDistance = distanceD;
+    totalMillis += currentMillis - previousMillis;
+    double displacement = equilibrium - distanceD;
+    Serial.print(totalMillis);
+    Serial.print(" ");
+    Serial.print(displacement, 3);
+    Serial.print(" ");
+    Serial.println(distanceD, 3);
+
+    if (displacement >= 0.038 && displacement <= 0.042) // If mass is extended roughly 4cm away, give go signal
+    {
+      digitalWrite(GO_LED, HIGH);
+    }
+    else
+    {
+      digitalWrite(GO_LED, LOW);
+    }
+  }
+  
+  previousMillis = currentMillis;
+  
+  delay(45);
+}
+
+float tempC() 
+{
+  float raw = analogRead(TMPPIN);
+  float percent = raw / 1024.0;
+  float volts = percent * 5.0;
+  return 100.0 * (volts - 0.5);
+}
+
+double distance()
+{
+  double microseconds = 0;
+  double distance = 0.0;
+  
+  microseconds = (double) sonar.ping_median(3) / 1000.0 / 1000.0 / 2.0;
+  distance = VELOCITY * microseconds;
+  
+  return distance;
+}
+
