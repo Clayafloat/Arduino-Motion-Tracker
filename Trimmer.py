0fedb00f669ce53187e88a113615496eadf7bdfa
clayton-halim <ClaytonHalim3@gmail.com>
2017-01-19 13:32:52 -0500

    Added documentation and removed all spreadsheets

--- Trimmer.py
+++ Trimmer.py
@@ -1,13 +1,17 @@
 def main(path, newFile):
+"""Processes and cleans the raw data collected from the arduino into a csv 
+
+	path -- path to raw data csv file
+	newFile -- file name to output
 """
-Processes and cleans the raw data collected from the arduino into a csv 
-"""
+
     f = open(path, 'r')
     w = open(newFile + ".csv", 'w')
 
+	TRIAL_TIME_SEC = 120.0
     record = {}
     prevTime = 0.0
-    largest = -9999999.9
+    largest = -9999999.9 
     temp = 0.0
 
     subtract = float(f.readline().split(',')[0])
@@ -26,7 +30,7 @@ Processes and cleans the raw data collected from the arduino into a csv
         elif pos == largest and pos:
             temp = pos
         else:
-            if (largest != -9999999.9 and float(prevTime) <= 120.0):
+            if (largest != -9999999.9 and float(prevTime) <= TRIAL_TIME_SEC):
                 w.writelines(str(float(prevTime) - subtract) + "," + str(largest) + "\n")
                 decay = largest
             largest = -9999999.9
--- pyserial.py
+++ pyserial.py
@@ -4,23 +4,23 @@ from time import strftime
 arduino = serial.Serial("COM3", 9600)
 data_file = None
 
-#Trial Data
-mass = 1.5 #kg
-k = 117
-
-def main(trial=1):    
+def main():
+  """Monitors the serial output from the Arduino and formats the recorded data into a csv."""
+  
+  trial = 1
+  
   while (arduino.isOpen()):
     line = arduino.readline()[:-2]
     
     if line == "COMMAND: start_recording":
-      print "STARTING RECORDING"
+      print("STARTING RECORDING")
       data_file = open("[DATA] " +strftime("%Y-%m-%d %H.%M.%S") + " trial " + str(trial) + ".csv", "w")
     elif line == "COMMAND: stop_recording":
       data_file.close()
-      print "FINSIHED RECORDING"
+      print("FINSIHED RECORDING")
       trial += 1
-    elif line[-1] == "C":
-      print line
+    elif line[-1] == "C": 
+      print(line)
     else:
       data = line.split(" ")
       time = round(float(data[0]) / 1000.0, 4)
@@ -29,7 +29,7 @@ def main(trial=1):
       if pos < 0.06 and pos > -0.06: #Prevents outliers
         write = str(time) + "," + str(pos) + "\n"
         data_file.write(write)
-        print write
+        print(write)
 
 if __name__ == '__main__':
   main()
